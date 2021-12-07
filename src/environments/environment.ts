d67128ff958be4b569b1562948a44e65e1033421
ddev8 <x.ddev8@gmail.com>
2021-12-07 09:22:41 +0200

    feat: firebase emulator for unit testing. build: docker files were changed for running emulator inside.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  emulator: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyD_ifia6H49yOZgZ9VmYF7ohoDgQBT5bjQ",
+    authDomain: "all-in-one-a3578.firebaseapp.com",
+    databaseURL: "https://localhost:8080", //"https://all-in-one-a3578-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "all-in-one-a3578",
+    storageBucket: "all-in-one-a3578.appspot.com",
+    messagingSenderId: "683592449043",
+    appId: "1:683592449043:web:58efd3f20acbebb29a53e6",
+    measurementId: "G-RKVDFRFRVG"
+  }
 };
 
 /*
