0ea87f2ee2d0b3f8f73775b94b310129f9d71727
ddev8 <x.ddev8@gmail.com>
2021-12-19 09:23:27 +0200

    feat: add pwa. dev-infra: fix docker-compose.

--- src/app/shared/nav-menu/nav-menu.component.spec.ts
+++ src/app/shared/nav-menu/nav-menu.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { PrimeNgModule } from '../prime-ng-module/prime-ng.module';
 
 import { NavMenuComponent } from './nav-menu.component';
 
@@ -8,7 +11,9 @@ describe('NavMenuComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavMenuComponent ]
+      imports: [ PrimeNgModule, RouterTestingModule.withRoutes([]), ],
+      declarations: [ NavMenuComponent ],
+      providers: []
     })
     .compileComponents();
   });
