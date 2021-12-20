95d7ac25ff7a3da1af75930e7c34fc207e3eba2d
ddev8 <x.ddev8@gmail.com>
2021-12-20 14:39:39 +0200

    feat: basic auth with google.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexPageComponent } from './index-page/index-page.component';
+import { SigninComponent } from './signin/signin.component';
 import { TextMemorizationComponent } from './text-memorization/text-memorization.component';
+import { AuthGuard, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+
+import { AngularFireAuthGuard } from '@angular/fire/compat/auth-guard';
 
 const routes: Routes = [
   {
     path: '',
     component: TextMemorizationComponent,
-  }
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: () => redirectUnauthorizedTo('login') }
+  },
+  {
+    path: 'login',
+    component: SigninComponent,
+  },
+  {
+    path: '**',
+    component: SigninComponent,
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PrimeNgModule } from './shared/prime-ng-module/prime-ng.module';
 import { TextMemorizationComponent } from './text-memorization/text-memorization.component';
-import { NavMenuComponent } from './shared/nav-menu/nav-menu.component';
 import { IndexPageComponent } from './index-page/index-page.component';
-import { FirestoreModule } from './shared/firestore-module/firestore.module';
-import { MemorizationService } from './shared/memorization.service';
 
 import { environment } from '../environments/environment';
+import { SigninComponent } from './signin/signin.component';
+import { AngularFireAuth, AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
     TextMemorizationComponent,
-    NavMenuComponent,
-    IndexPageComponent
+    IndexPageComponent,
+    SigninComponent,
   ],
   imports: [
+    AngularFireAuthModule,
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    PrimeNgModule,
     FormsModule,
     ReactiveFormsModule,
-    FirestoreModule,
+    SharedModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
@@ -40,7 +37,7 @@ import { environment } from '../environments/environment';
     }),
   ],
   providers: [
-    MemorizationService,
+    // AuthGuard,
   ],
   bootstrap: [AppComponent]
 })
