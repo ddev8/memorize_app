b05bf3d565b81d2fa429ac47624fc29f4a1fbfc5
ddev8 <yavorsky.dan@gmail.com>
2022-02-20 22:08:08 +0200

    feat: basic auth state, actions,reducer.

---
+++ src/app/auth/store/actions/check-auth.actions.ts
@@ -0,0 +1,16 @@
+import { createAction, props } from '@ngrx/store';
+import { User } from '../../models/user';
+
+export const loadCheckAuth = createAction(
+  '[CheckAuth] Load CheckAuth'
+);
+
+export const loadCheckAuthSuccess = createAction(
+  '[CheckAuth] Load CheckAuth Success',
+  props<{ data: { user: User } }>()
+);
+
+export const loadCheckAuthFailure = createAction(
+  '[CheckAuth] Load CheckAuth Failure',
+  props<{ error: string }>()
+);
