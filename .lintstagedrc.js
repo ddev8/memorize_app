d9eb65551de3feb89c298a61308b4f013e7da7de
ddev8 <x.ddev8@gmail.com>
2022-08-03 22:36:01 +0300

    feat: pre commit hook for staged files.

--- .lintstagedrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  'src/**/*.ts': (files) => `ng lint ${files.map((file) => `--lint-file-patterns ${file}`).join(' ')}`
-  // 'src/**/*.spec.ts': (files) =>
-  //   `ng test --watch false --browsers ChromeHeadless ${files.map((file) => `--include ${file}`).join(' ')}`
-};
